c3d2137d1f185c7a1d795d4ed304f17e9c44146d
sherchansachin <sachinsherchan3@gmail.com>
2021-07-24 23:03:31 +0545

    crispy forms

--- alpinetours/settings.py
+++ alpinetours/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +44,10 @@ INSTALLED_APPS = [
     'accounts',
     'main',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
