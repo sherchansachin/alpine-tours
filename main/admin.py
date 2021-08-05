fc9171380c6063238925548e21929a29463f5753
sherchansachin <sachinsherchan3@gmail.com>
2021-08-05 23:22:13 +0545

    made model for packages and registered

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Packages
 # Register your models here.
+
+
+admin.site.register(Packages)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
+from decouple import Choices
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
+PACKAGE_STATUS = (
+    (0, 'Unavailable'),
+    (1, 'Available')
+)
+
+class Packages(models.Model):
+    """ model to store packages information to DB """
+    package_title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=50)
+    days = models.CharField(max_length=50)
+    image = models.ImageField(upload_to ='package_images')
+    description =  RichTextUploadingField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    status = models.IntegerField(choices=PACKAGE_STATUS, default=0) # 0 -> package is unavailable
+
+    def __str__(self):
+        return self.package_title
