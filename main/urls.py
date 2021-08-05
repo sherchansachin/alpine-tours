eeafd5b1c3b9e099a9bcc8b13824c4fca4a688e6
sherchansachin <sachinsherchan3@gmail.com>
2021-08-05 23:27:24 +0545

    views and url paths configured

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [ 
     path('', views.index, name='index'),
+    path('packages/', views.packages, name='packages'),
+    path('package-details/<slug:slug>/', views.package_detail, name='package_info'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from main.models import Packages
+from django.shortcuts import render, get_object_or_404
+from .models import Packages
 
 # Create your views here.
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def packages(request):
+    packages = Packages.objects.all()
+    return render(request, 'main/packages.html', {"packages":packages})
+
+def package_detail(request, slug):
+    info = get_object_or_404(Packages, slug=slug)
+    return render(request, 'main/package_info.html', {'info':info})
