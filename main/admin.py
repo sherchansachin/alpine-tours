ee95e9e6b74f114bd95f12a2eb6972ba6edc77ab
sherchansachin <sachinsherchan3@gmail.com>
2021-08-14 23:46:51 +0545

    added comment model

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Packages
+from django.contrib.admin.decorators import action
+from .models import Packages, Comment
 # Register your models here.
 
 
 admin.site.register(Packages)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'comment', 'posted_on', 'active')
+    list_filter = ('active', 'posted_on')
+    search_fields = ('name', 'comment')
+    actions = ['approve_comments']
+
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active = True)
--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from decouple import Choices
 from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.db.models.deletion import CASCADE
+from django.utils.translation import activate
 
 # Create your models here.
 PACKAGE_STATUS = (
@@ -20,3 +22,18 @@ class Packages(models.Model):
 
     def __str__(self):
         return self.package_title
+
+
+class Comment(models.Model):
+    """ model to store and accept comments for packages """
+    post = models.ForeignKey(Packages, related_name='comments', on_delete=CASCADE)
+    name = models.CharField(max_length=150)
+    comment = models.TextField()
+    posted_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['posted_on']
+
+    def __str__(self):
+        return '{} posted a comment {}'.format(self.name, self.comment)
